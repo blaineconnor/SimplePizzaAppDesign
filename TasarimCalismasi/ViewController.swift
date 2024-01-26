d3394d2ea049bf99cb2c50e9b7fd9a4917d4666a
blaineconnor <112892044+blaineconnor@users.noreply.github.com>
2024-01-26 17:14:08 +0300

    Final

--- TasarimCalismasi/ViewController.swift
+++ TasarimCalismasi/ViewController.swift
@@ -9,9 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.navigationItem.title = "Pizza"
+        
+        let appearance = UINavigationBarAppearance()
+            
+        appearance.backgroundColor = UIColor(named: "anaRenk")
+        appearance.titleTextAttributes = [.foregroundColor : UIColor(named: "yaziRenk")!, 
+            .font:UIFont(name: "Pacifico-Regular", size: 22)!]
+        navigationController?.navigationBar.barStyle = .black
+        
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.compactAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
     }
 
 
